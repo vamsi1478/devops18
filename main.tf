951b0f1d1e4abac6e2835f88729b91995206886c
vamsi1478 <140262716+vamsi1478@users.noreply.github.com>
2024-02-29 16:02:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_configuration" "web_server_as" {
-    image_id           = "ami-00b8917ae86a424c9"
+    image_id           = "ami-0c7f9161f8491665f"
     instance_type = "t2.micro"
-    key_name = "ansible-kp"
+    key_name = "vamsi.pem"
 }
    
 
@@ -28,7 +28,7 @@ resource "aws_autoscaling_group" "web_server_asg" {
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = ["us-east-1a", "us-east-1b"] 
+    availability_zones    = ["us-east-2c", "us-east-2a"] 
     
   }
 
