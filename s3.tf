3d6bd000b83eb1bda780de1d20f4d9ea62495ca6
vamsi1478 <140262716+vamsi1478@users.noreply.github.com>
2024-03-01 22:01:13 +0530

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,27 @@
+resource "aws_s3_bucket_ownership_controls" "two" {
+  bucket = aws_s3_bucket.one.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "three" {
+  depends_on = [aws_s3_bucket_ownership_controls.two]
+
+  bucket = aws_s3_bucket.one.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "three" {
+bucket = aws_s3_bucket.one.id
+versioning_configuration {
+status = "Enabled"
+}
+}
+terraform {
+  backend "s3" {
+    bucket = "sai7243.flm.bucket"
+    key    = "prod/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
